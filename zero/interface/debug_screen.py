0f613d01ecb0db3c1b04383d064b1d187d40c33b
Maciej Sokołowski <matemaciek@gmail.com>
2021-10-17 11:07:55 +0200

    Show IP for future remote config

--- zero/interface/debug_screen.py
+++ zero/interface/debug_screen.py
@@ -1,11 +1,18 @@
+import socket
+
 import interface.ui
 from interface.buttons import Command
-from interface.ui import ScreenCommand, BKG, FNT, FNT_BASE, D
+from interface.ui import ScreenCommand, BKG, FNT, FNT_BASE
+
+def ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 1))
+    return s.getsockname()[0]
 
 class DebugScreen(interface.ui.Screen):
     def _start(self):
         self._draw.rectangle((0, 0, self._W, self._H), fill=BKG)
-        text = "Up:  shutdown\nEnter: reboot\nDown: restart"
+        text = "IP: {}\nUp / Back:  shutdown\nEnter: reboot\nDown: restart".format(ip())
         (box_x, box_y) = self._draw.textsize(text, **FNT_BASE)
         self._draw.text(((self._W - box_x)//2, (self._H - box_y)//2), text, **FNT)
 
